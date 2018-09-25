38dee89bf0f8d184e298be7505c6bda4412e01ff
kimorac20 <ulacit@docente.ulacit.ac.cr>
2018-09-24 18:59:08 -0600

    Kim

---
+++ Lab03/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab03
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public static string GetStaticName()
+        {
+            return Name;
+        }
+    }
+}
